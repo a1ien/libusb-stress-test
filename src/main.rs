2c313148cbc4688aee23ffe715f910c67973b828
Ilya Averyanov <i.averyanov@geoscan.aero>
2022-04-12 11:49:56 +0300

    Init tests

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use libusb1_sys as ffi;
+
+fn main() {
+    let mut context: *mut ffi::libusb_context = std::ptr::null_mut();
+    for i in 0..=100 {
+        match unsafe { ffi::libusb_init(&mut context) } {
+            0 => (),
+            err => panic!("Failed to init libusb on iteration {}: {}", i, err),
+        }
+        unsafe {
+            ffi::libusb_exit(context);
+        }
+        context = std::ptr::null_mut();
+    }
+}
