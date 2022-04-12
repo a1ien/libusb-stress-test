3af51f3b75c1dceb2b42063cecfd647c04d847c6
Ilya Averyanov <i.averyanov@geoscan.aero>
2022-04-12 11:56:04 +0300

    Release and more runs

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use libusb1_sys as ffi;
 
 fn main() {
     let mut context: *mut ffi::libusb_context = std::ptr::null_mut();
-    for i in 0..=100 {
+    for i in 0..=1000 {
         match unsafe { ffi::libusb_init(&mut context) } {
             0 => (),
             err => panic!("Failed to init libusb on iteration {}: {}", i, err),
